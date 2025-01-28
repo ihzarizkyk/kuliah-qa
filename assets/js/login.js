667796c783cbfb3ae7d44d0983783c20dff3b142
ihzarizkyk <ihzarizky30@gmail.com>
2025-01-28 20:23:21 +0700

    Update LoginTC

---
+++ assets/js/login.js
@@ -0,0 +1,13 @@
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const login = document.getElementById("login");
+
+login.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (username.value === "admin123" && password.value === "12345678") {
+        window.location.href = "loged.html";
+    } else {
+        alert("Username atau password salah. Silakan coba lagi.");
+    }
+});
